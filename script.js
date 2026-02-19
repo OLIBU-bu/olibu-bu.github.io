7448140446283021a21e9fc08581591afe58246b
Olibu <juancar29082002@gmail.com>
2026-02-19 16:33:17 -0500

    script.js

--- script.js
+++ script.js
@@ -202,4 +202,16 @@ document.addEventListener("click", function(e) {
 
 });
 
+/* EFECTO NAVBAR SCROLL */
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+
+  if (!navbar) return;
+
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
 
